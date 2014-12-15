d2fe8ca85a690a86b76f69eadc0f490eab853c4f
John Rapp <johnfarnes@hotmail.com>
2014-12-15 02:26:32 +0100

    Very important label

--- Matteprogrammering/Matteprogrammering/GraphViewer.Designer.cs
+++ Matteprogrammering/Matteprogrammering/GraphViewer.Designer.cs
@@ -40,12 +40,13 @@ namespace Matteprogrammering
 			this.FunctionPicker = new Matteprogrammering.UI.FunctionPicker();
 			this.WindowController = new Matteprogrammering.UI.WindowController();
 			this.Graph = new Matteprogrammering.InteractiveGraph();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 373);
+			this.label1.Location = new System.Drawing.Point(12, 384);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(35, 13);
 			this.label1.TabIndex = 28;
@@ -56,7 +57,7 @@ namespace Matteprogrammering
 			this.checkBox1.AutoSize = true;
 			this.checkBox1.Checked = true;
 			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.checkBox1.Location = new System.Drawing.Point(15, 389);
+			this.checkBox1.Location = new System.Drawing.Point(15, 400);
 			this.checkBox1.Name = "checkBox1";
 			this.checkBox1.Size = new System.Drawing.Size(67, 17);
 			this.checkBox1.TabIndex = 34;
@@ -68,7 +69,7 @@ namespace Matteprogrammering
 			// checkBox2
 			// 
 			this.checkBox2.AutoSize = true;
-			this.checkBox2.Location = new System.Drawing.Point(88, 389);
+			this.checkBox2.Location = new System.Drawing.Point(88, 400);
 			this.checkBox2.Name = "checkBox2";
 			this.checkBox2.Size = new System.Drawing.Size(66, 17);
 			this.checkBox2.TabIndex = 35;
@@ -80,7 +81,7 @@ namespace Matteprogrammering
 			// checkBox3
 			// 
 			this.checkBox3.AutoSize = true;
-			this.checkBox3.Location = new System.Drawing.Point(161, 389);
+			this.checkBox3.Location = new System.Drawing.Point(161, 400);
 			this.checkBox3.Name = "checkBox3";
 			this.checkBox3.Size = new System.Drawing.Size(54, 17);
 			this.checkBox3.TabIndex = 36;
@@ -92,7 +93,7 @@ namespace Matteprogrammering
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(12, 409);
+			this.label3.Location = new System.Drawing.Point(12, 420);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(44, 13);
 			this.label3.TabIndex = 37;
@@ -101,7 +102,7 @@ namespace Matteprogrammering
 			// radioButton3
 			// 
 			this.radioButton3.AutoSize = true;
-			this.radioButton3.Location = new System.Drawing.Point(216, 426);
+			this.radioButton3.Location = new System.Drawing.Point(216, 437);
 			this.radioButton3.Name = "radioButton3";
 			this.radioButton3.Size = new System.Drawing.Size(65, 17);
 			this.radioButton3.TabIndex = 40;
@@ -113,7 +114,7 @@ namespace Matteprogrammering
 			// radioButton2
 			// 
 			this.radioButton2.AutoSize = true;
-			this.radioButton2.Location = new System.Drawing.Point(118, 426);
+			this.radioButton2.Location = new System.Drawing.Point(118, 437);
 			this.radioButton2.Name = "radioButton2";
 			this.radioButton2.Size = new System.Drawing.Size(96, 17);
 			this.radioButton2.TabIndex = 39;
@@ -126,7 +127,7 @@ namespace Matteprogrammering
 			// 
 			this.radioButton1.AutoSize = true;
 			this.radioButton1.Checked = true;
-			this.radioButton1.Location = new System.Drawing.Point(15, 426);
+			this.radioButton1.Location = new System.Drawing.Point(15, 437);
 			this.radioButton1.Name = "radioButton1";
 			this.radioButton1.Size = new System.Drawing.Size(97, 17);
 			this.radioButton1.TabIndex = 38;
@@ -138,15 +139,15 @@ namespace Matteprogrammering
 			// 
 			// FunctionPicker
 			// 
-			this.FunctionPicker.Location = new System.Drawing.Point(12, 318);
+			this.FunctionPicker.Location = new System.Drawing.Point(12, 331);
 			this.FunctionPicker.Name = "FunctionPicker";
-			this.FunctionPicker.Size = new System.Drawing.Size(328, 52);
+			this.FunctionPicker.Size = new System.Drawing.Size(269, 52);
 			this.FunctionPicker.TabIndex = 26;
 			// 
-			// WindowPicker
+			// WindowController
 			// 
 			this.WindowController.Location = new System.Drawing.Point(518, 12);
-			this.WindowController.Name = "WindowPicker";
+			this.WindowController.Name = "WindowController";
 			this.WindowController.Size = new System.Drawing.Size(163, 184);
 			this.WindowController.TabIndex = 25;
 			this.WindowController.Window = null;
@@ -159,11 +160,21 @@ namespace Matteprogrammering
 			this.Graph.Size = new System.Drawing.Size(500, 300);
 			this.Graph.TabIndex = 11;
 			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 315);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(51, 13);
+			this.label2.TabIndex = 41;
+			this.label2.Text = "Function:";
+			// 
 			// GraphViewer
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(692, 502);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.radioButton3);
 			this.Controls.Add(this.radioButton2);
 			this.Controls.Add(this.radioButton1);
@@ -195,6 +206,7 @@ namespace Matteprogrammering
 		private System.Windows.Forms.RadioButton radioButton1;
 		private System.Windows.Forms.RadioButton radioButton2;
 		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.Label label2;
 
 	}
 }
